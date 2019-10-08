8e31e1dc1eb62868eb9f811340ca9cb634decc5a
810shoma <ow8100@gmail.com>
2019-10-08 15:42:51 +0900

    ・スペースを押すとキャラが切り替わる ・簡単な背景や地面を生成

---
+++ Assets/goya/Script/Mosion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mosion : MonoBehaviour {
+
+    [SerializeField,Header("待機状態")]
+    private GameObject Normal;
+    [SerializeField, Header("攻撃モーション")]
+    private GameObject Attack;
+    [SerializeField, Header("竹")]
+    private GameObject Target;
+    bool Cooltime;  //攻撃できるかの状態（TRUE：できる、FALSE：できない）
+
+    void Start () {
+        Cooltime = true;
+
+	}
+	
+	void Update () {
+        if (Input.GetKey("space"))
+        {
+            Attack.transform.position = new Vector3(0, 0, 0);
+            Normal.transform.position = new Vector3(0, 10, 0);
+
+            //Destroy(Target);
+            GameObject obj = (GameObject)Resources.Load("beam_pink");
+            Instantiate(obj, new Vector3(0.0f, 2.0f, 0.0f), Quaternion.identity);
+        }
+        else
+        {
+            Normal.transform.position = new Vector3(0, 0, 0);
+            Attack.transform.position = new Vector3(0, 10, 0);
+        }
+    }
+}
