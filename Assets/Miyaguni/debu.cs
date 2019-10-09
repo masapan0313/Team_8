c42c0caef1f52e9bc8fa2eccc6115f62a5933980
I_am_4Bit <11807037@stu.kbc.ac.jp>
2019-10-09 11:31:10 +0900

    竹が上に伸び続ける。 画面外で削除、画面下で再生成

---
+++ Assets/Miyaguni/debu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debu : MonoBehaviour {
+    public GameObject p;
+    void start()
+    {
+
+    }
+    void Update()
+    {
+
+        this.gameObject.transform.Translate(0, 0.05f, 0);
+
+        if (transform.position.y > 30)
+        {
+            p = GameObject.Find("GameObject d");
+            p.GetComponent<manage>().d = true;
+            Destroy(gameObject);
+
+        }
+
+    }
+
+
+}
---
+++ Assets/Miyaguni/manage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manage : MonoBehaviour
+{
+    public bool d = false;
+    public bool z = false;
+    public bool h = false;
+    public GameObject debu;
+    public GameObject Zako;
+    public GameObject point;
+    public GameObject hayai;
+    public GameObject points;
+
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (d == true)
+        {
+            GameObject o = new GameObject();
+            o = GameObject.Instantiate(debu, this.transform);
+            d = false;
+
+        }
+        if (z == true)
+        {
+            GameObject t = new GameObject();
+            t = GameObject.Instantiate(Zako, point.transform);
+            z = false;
+        }
+        if (h == true)
+        {
+            GameObject r = new GameObject();
+            r = GameObject.Instantiate(hayai, points.transform);
+            h = false;
+        }
+
+    }
+}
