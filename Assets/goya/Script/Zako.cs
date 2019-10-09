2a9fd26a529031bbfc97d673f67f9416d27b7747
810shoma <ow8100@gmail.com>
2019-10-09 11:40:23 +0900

    Merge remote-tracking branch 'origin/rinya' into Shoma
    # Conflicts:
    #	Assembly-CSharp.csproj
    #	Assets/Miyaguni/delete.cs.meta
    #	Assets/MoveBamboo.cs
    #	Assets/MoveBamboo.cs.meta
    #	Assets/goya/Script/Mosion.cs.meta
    #	Assets/goya/Script/Slashing.cs.meta
    #	Assets/goya/Script/hayai.cs.meta
    #	Assets/goya/Script/manage.cs.meta
    #	Assets/goya/delete.cs.meta
    #	Library/CurrentLayout.dwlt
    #	Library/ScriptAssemblies/Assembly-CSharp.dll
    #	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	Library/ScriptAssemblies/Unity.PackageManagerUI.Editor.dll.mdb
    #	Library/ScriptAssemblies/UnityEditor.StandardEvents.dll.mdb
    #	Library/assetDatabase3
    #	Library/metadata/0d/0d3bb855445e36e479c85976fc88383a
    #	Library/metadata/12/12fd8a0055b84bb59e84c9835a37e333
    #	Library/metadata/1b/1b355da31ea043feba0c03e35dd4bc62
    #	Library/metadata/1c/1c6d1fbb51834b64847b1b73a75bfc77
    #	Library/metadata/21/21eff446d50eaf44a85985cd4c0b6fa1
    #	Library/metadata/26/2682a692a2be7e14e901a738c7806da0
    #	Library/metadata/2f/2fe3476eabbbb6c448e6b55a2cc471f5
    #	Library/metadata/32/32188fd89022c154c81befa2f0e00be0
    #	Library/metadata/32/328cc881519068e4eb7db4bb907ad2d9
    #	Library/metadata/38/38c8faf1788024c02930a0c68a6e0edc
    #	Library/metadata/3a/3a84de5cd0624681b6b6dcd8921d912a
    #	Library/metadata/3b/3bb0254b9671413d9d08a855c5090237
    #	Library/metadata/3e/3e9a2175e0404f7ea30b2b694850bae0
    #	Library/metadata/40/405b9b51bb344a128608d968297df79c
    #	Library/metadata/41/4113173d5e95493ab8765d7b08371de4
    #	Library/metadata/46/46098a81a64a45c5b756320e10a22f13
    #	Library/metadata/49/49f5766d0d4954f44b85d4bbd7131677
    #	Library/metadata/4a/4a3ecb1425d14502837abea459cf2b70
    #	Library/metadata/4b/4b3fa4bde7f1451a8218c03ee6a8ded8
    #	Library/metadata/4b/4ba2329b63d54f0187bcaa12486b1b0f
    #	Library/metadata/51/517af1b5b81b93b43b9745d58f017562
    #	Library/metadata/53/53ebcfaa2e1e4e2dbc85882cd5a73fa1
    #	Library/metadata/57/5782f9e9e6e0bb94bac99aeea24814fc
    #	Library/metadata/5f/5f32cd94baa94578a686d4b9d6b660f7
    #	Library/metadata/69/6981461fe431401459211818212a29cf
    #	Library/metadata/6a/6a10b2909283487f913b00d94cd3faf5
    #	Library/metadata/6e/6e2d432c5ea14e75aa3a79f90f704442
    #	Library/metadata/6f/6fe576b47d8b47688ec5d02bccd15cf9
    #	Library/metadata/76/7668179ede524d6396c8b7d84461ea29
    #	Library/metadata/80/80a3616ca19596e4da0f10f14d241e9f
    #	Library/metadata/84/844f815391db42d49455cbf1a7bfc434
    #	Library/metadata/84/84ca94c19f25ae14d83aa41bb3654390
    #	Library/metadata/d9/d91035c548f23744c9bfb107348ed1c0
    #	Library/metadata/ea/ead147da21254ff9a0a936bdd75e1680
    #	Library/metadata/ed/ed7343f30e3843b3afda8f8b02669cea
    #	Library/metadata/f5/f5f67c52d1564df4a8936ccd202a3bd8
    #	Library/metadata/f7/f70555f144d8491a825f0804e09c671c
    #	Library/shadercompiler-UnityShaderCompiler.exe0.log

---
+++ Assets/goya/Script/Zako.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zako : MonoBehaviour {
+    public GameObject p;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Translate(0, 0.05f, 0);
+
+        if (transform.position.y > 30)
+        {
+            p = GameObject.Find("GameObject d");
+            p.GetComponent<manage>().z = true;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/goya/Script/hayai.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hayai : MonoBehaviour {
+    public GameObject p;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Translate(0, 0.08f, 0);
+
+        if (transform.position.y > 30)
+        {
+            p = GameObject.Find("GameObject d");
+            p.GetComponent<manage>().h = true;
+            Destroy(gameObject);
+        }
+    }
+}
