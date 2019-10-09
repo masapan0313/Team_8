6bacbddd44839e35a5e5439d4f3fc271f4a5019c
オルミアナ <yuta0518yuta@gmail.com>
2019-10-09 15:49:29 +0900

    UI

---
+++ Assets/karimata/LifeGauge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeGauge : MonoBehaviour {
+
+    public RawImage[] image ;
+    int count = 2;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+           image[count].enabled = false;
+           // Debug.Log(image[count]);
+            count--;
+        }
+
+        if (count == -1)
+        {
+            Debug.Log("atatatatatatattata");
+        }
+    }
+}
---
+++ Assets/karimata/PlayerController1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController1 : MonoBehaviour
+{
+   
+    public GameObject player;
+    public GameObject ber;
+    Slider hpSlider;
+
+    public Transform Pacon;
+    float playerpos;
+    void Start()
+    {
+        playerpos = player.transform.position.x;
+    }
+
+    void Update()
+    {
+
+        Pacon.position += new Vector3(0.1f, 0);
+       
+        if(playerpos +10f < player.transform.position.x)
+        {
+            ber.GetComponent<Slider>().value -= 0.01f;
+            playerpos = player.transform.position.x;
+        }
+    }
+}
+   
