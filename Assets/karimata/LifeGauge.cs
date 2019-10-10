d0e2a14b37d43a70e9295add7a6358d3f07b78d6
オルミアナ <yuta0518yuta@gmail.com>
2019-10-10 11:22:17 +0900

    リトライの処理

--- Assets/karimata/LifeGauge.cs
+++ Assets/karimata/LifeGauge.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LifeGauge : MonoBehaviour {
 
-    public RawImage[] image ;
+    public Text failText;
+   // private bool inGame;
+
+    public RawImage[] image;
     int count = 2;
+
+    void Start()
+    {
+        failText.enabled = false;
+        //inGame = true;
+    }
 
     void Update()
     {
@@ -15,11 +25,22 @@ public class LifeGauge : MonoBehaviour {
            image[count].enabled = false;
            // Debug.Log(image[count]);
             count--;
+            
         }
-
         if (count == -1)
         {
-            Debug.Log("atatatatatatattata");
+            // Debug.Log("GAME OVER");
+            failText.enabled = true;
+            // inGame = false;
+            if (Input.GetMouseButtonUp(1))
+            {
+                Replay();
+            }
         }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("ka");
     }
 }
